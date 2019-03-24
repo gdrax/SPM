954346083853c4e39c6265c3ffdd7a958c7b1df9
Gioele Bertoncini <gioele.bert@gmail.com>
2019-03-24 15:43:31 +0100

    ob

--- Assignment_2/farm.cpp
+++ Assignment_2/farm.cpp
@@ -1,26 +1,59 @@
 #include <iostream>
 #include <thread>
 #include <mutex>
+#include <string>
+#include <functional>
 
 #include "queue.hpp"
 
-int nw;
+using namespace std;
 
-boolean is_prime(int n) {
-  for (i=2; i<= n/2; i++) {
+/*
+Calculate if a number is prime
+*/
+bool is_prime(int n) {
+  for (int i=2; i<= n/2; i++) {
     if (n%i == 0)
       return false;
   }
   return true;
 }
 
-int worker(int n) {
-  int count_primes = 0;
-  for (i=n; i>1; i++) {
-    if (is_prime(i))
-      count_primes++;
+/*
+Structure of a worker thread
+*/
+template<typename input, typename output> class Worker {
+private:
+  function<output(input)> work;
+  g_queue<input> inQ;
+  g_queue<output> outQ;
+  string name;
+
+public:
+  Worker(string name, function<output(input)> f, g_queue<input> &inQ, g_queue<output> &outQ):
+    name(name), work(f), inQ(inQ), outQ(outQ) {}
+
+  thread *run() {
+    auto body = [&] () {
+
+      auto item = inQ.pop();
+
+      while(item > 0) {
+        auto result = work(item);
+
+        outQ.push(result);
+
+        //this_thread::sleep_for(100ms);
+
+        item = inQ.pop();
+      }
+      return;
+    };
+
+    auto tid = new thread(body);
+    return tid;
   }
-}
+};
 
 int main(int argc, char const *argv[]) {
 
@@ -29,10 +62,41 @@ int main(int argc, char const *argv[]) {
     return -1;
   }
 
-  nw = atoi(argv[1]);
+  int nw = atoi(argv[1]);
   int n = atoi(argv[2]);
 
-  
+  g_queue<int> inputQ;
+  g_queue<int> outputQ;
+
+  /*
+  Counts number of prime numbers in range [2, n]
+  */
+  auto count_primes = [] (int n) {
+    int count = 0;
+    for (int i=n; i>1; i--) {
+      if (is_prime(i))
+        count++;
+    }
+  };
+
+  for (int i=0; i<n; i++) {
+    inputQ.push(50);
+  }
+
+  vector<Worker<int, int>> workers;
+  vector<thread*> threads;
+
+  for (int i=0; i<nw; i++) {
+    workers.push_back(new Worker<int, int>("worker_"+i, count_primes, inputQ, outputQ));
+  }
+
+  for (int i=0; i<nw; i++) {
+    inputQ.push(0);
+  }
+
+  for (auto &t: threads) {
+    t->join();
+  }
 
   return 0;
 }
