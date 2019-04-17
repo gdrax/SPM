3ddf950f0f898c27ffaa4ba031f371c4b07ddcfa
gdrax <gioele.bert@gmail.com>
2019-04-17 11:14:49 +0200

    more

--- Assignment_3/game_of_life.cpp
+++ Assignment_3/game_of_life.cpp
@@ -10,25 +10,27 @@ using namespace cimg_library;
 using namespace std;
 
 int n = 0;
+int stop = 0;
 /*
 Structure of a worker thread
 */
 
 class Worker {
 private:
-  bool **matrices;
+  bool ***matrices;
   int start;
   int end;
   int name;
 
 public:
-  Worker(int name, bool **matrices, int start, int end):
+  Worker(int name, bool ***matrices, int start, int end):
     name(name), matrices(matrices), start(start), end(end) {}
 
   thread *run() {
     auto body = [&] () {
-      n=0;
+      while (!stop) {
 
+      }
       return;
       };
 
@@ -36,40 +38,69 @@ public:
   }
 };
 
+class Drawer {
+private:
+  bool ***matrices;
+
+public:
+  Drawer(bool ***matrices):
+    matrices(matrices) {}
+
+  thread *run() {
+    auto body = [&] () {
+      CImg<unsigned char> board(n,n,1,3,0);
+      CImgDisplay draw_disp(board,"Game of life");
+      while(!draw_disp.is_closed()) {
+        for (int i=0; i<n; i++) {
+          for (int j=0; j<n; j++) {
+            if (matrices[i][j][0] == true) {
+              board(i, j, 1) = 255;
+              board.display(draw_disp);
+            }
+          }
+        }
+      }
+      stop = 1;
+      return;
+    };
+    return new thread(body);
+  }
+};
+
 
 int main(int argc, char const *argv[]) {
   if (argc != 4) {
-    cout << argv[0] << " [N. WORKERS] [SIZE OF MATRICES] [INITIALIZATION]\n[INITIALIZATION]\n0 : random\n1 : arrows";
+    cout << argv[0] << " [N. WORKERS] [SIZE OF MATRICES] [INITIALIZATION]\n[INITIALIZATION] --> 0 : random, 1 : arrows\n";
     return -1;
   }
 
   int nw = atoi(argv[1]);
   n = atoi(argv[2]);
+  int const dim = n;
   int init = atoi(argv[3]);
 
-  bool matrices[n][n][2];
+  bool ***matrices = new bool[dim][dim][2];
   for (int i=0; i<n; i++) {
-    for (int j=0; j<100; j++) {
-      matrices[i][j][0];
-      matrices[i][j][1];
+    for (int j=0; j<n; j++) {
+      matrices[i][j][0] = 0;
+      matrices[i][j][1] = 0;
     }
   }
 
+  vector<thread *> threads;
+  vector<Worker *> workers;
+  Drawer *drawer = new Drawer(matrices);
+  for (int i=0; i<nw; i++) {
+    workers.push_back(new Worker(i, matrices, 0+n/nw*i, (n/nw-1)+n/nw*i));
+  }
 
-  CImg<unsigned char> visu(200,200,1,3,0);
-  const unsigned char red[] = { 255,0,0 }, green[] = { 0,255,0 }, blue[] = { 0,0,255 };
-  CImgDisplay draw_disp(visu,"Intensity profile");
-  int i=0;
-  int j=0;
-  while (!draw_disp.is_closed()) {
-    if (i<200)
-      i+=4;
-    else {
-      i=0;
-      j+=3;
-    }
-    visu(j, i, 1) = 255;
-    visu.display(draw_disp);
+  threads.push_back(drawer->run());
+  for (auto w: workers) {
+    threads.push_back(w->run());
+  }
+
+  for (auto t: threads) {
+    t->join();
   }
   return 0;
 }
