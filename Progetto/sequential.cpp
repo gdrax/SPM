263126b2048066b81ee2047f432df01116265c27
gdrax <gioele.bert@gmail.com>
2019-07-17 00:02:13 +0200

    sequential works

--- Progetto/sequential.cpp
+++ Progetto/sequential.cpp
@@ -4,8 +4,8 @@
 using namespace std;
 
 int main(int argc, char const *argv[]) {
-    if (argc < 4) {
-        cout << "USAGE: " << argv[0] << " [function_name] [n. of particles] [n. iterations]\n";
+    if (argc < 5) {
+        cout << "USAGE: " << argv[0] << " [function_name] [init_type] [n. of particles] [n. iterations]\n";
         return -1;
     }
 
@@ -15,29 +15,33 @@ int main(int argc, char const *argv[]) {
         return -1;
     }
 
-    int n_particles = atoi(argv[2]);
+    string init_type = argv[2];
+    if (!check_init_type(init_type)) {
+        cout << "ERROR: init_type is not valid. The available initialization are: \"uniform\", \"random\"\n";
+        return -1;
+    }
+
+    int n_particles = atoi(argv[3]);
     if (n_particles <= 0) {
         cout << "ERROR: n. of particles must be positive\n";
         return -1;
     }
 
     //in realtà must be quadrato perfetto sargio
-    int epochs = atoi(argv[3]);
+    int epochs = atoi(argv[4]);
     if (epochs <= 0) {
         cout << "ERROR: n. of iterations must be positive\n";
     }
 
-    swarm_t *swarm = initialize_swarm(n_particles, target_func);
+    swarm_t *swarm = init_swarm(n_particles, target_func, init_type);
 
     print_swarm(swarm, target_func);
 
     for (int i=0; i<epochs; i++) {
         cout << "Iteration" << i << ": global_min = " << compute_bench_fun(swarm->global_min, target_func) << "\n";
         for (int j=0; j<swarm->n_particles; j++) {
-            update_velocity(&(swarm->particles[j]), swarm->global_min);
-            cout << (swarm->particles[j].position.x) << endl;
+            update_velocity(&(swarm->particles[j]), swarm->global_min, target_func);
             update_local(&(swarm->particles[j]), target_func);
-            cout << (swarm->particles[j].position.x) << "sargio\n";
         }
         update_global(swarm, target_func);
     }
--- Progetto/utils.hpp
+++ Progetto/utils.hpp
@@ -34,17 +34,26 @@ typedef struct {
 /* Domain bounds for the benchmark functions */
 float const sphere_domain_bound = 100;
 float const himmel_domain_bound = 5;
+float const matyas_domain_bound = 10;
 
 /* Default parameters to compute the velocity */
 float const cognitive_parameter = 2;
 float const social_parameter = 2;
 float const inertia_weight = 0.9;
+float const velocity_clamp = 0.5;
 
 /**
  * Generates a random number in [0, 1] sargio
 **/
 float random01() {
     return (float)((float)rand() / (float)(RAND_MAX));
+}
+
+/**
+ * Generates a random number in [a, b] sargio
+ */
+float randAB(float a, float b) {
+    return a + random01() * (b-a);
 }
 
 /**
@@ -57,6 +66,9 @@ float compute_bench_fun(position_t p, string func) {
     if (func == "himmel") {
         return pow(pow(p.x, 2) + p.y + 11, 2) + pow(p.x + pow(p.y, 2) + 7, 2);
     }
+    if (func == "matyas") {
+        return 0.26 * (pow(p.x, 2) + pow(p.y, 2)) - 0.48 * p.x * p.y;
+    }
     return 0;
 }
 
@@ -64,10 +76,20 @@ float compute_bench_fun(position_t p, string func) {
  * check if the bechmark function name is valid
  */
 bool check_bench_fun(string func) {
-    if (func == "sphere" || func == "himmel")
+    if (func == "sphere" || func == "himmel" || func == "matyas")
         return true;
     else
         return false;
+}
+
+/**
+ * check if the init type is valid
+ */
+bool check_init_type(string init) {
+    if (init == "uniform" || init == "random")
+        return true;
+    else
+        return false;    
 }
 
 /**
@@ -78,37 +100,39 @@ float bench_fun_bound(string func) {
         return sphere_domain_bound;
     if (func == "himmel")
         return himmel_domain_bound;
+    if (func == "matyas")
+        return matyas_domain_bound;
     return 0;
 }
 
 /**
  * Compute the new X component of the velocity of a particle
 **/
-void compute_velocity_X(particle_t *particle, position_t global_min) {
+void compute_velocity_X(particle_t *particle, position_t global_min, string func) {
     float local_diff = particle->local_min.x - particle->position.x;
     float global_diff = global_min.x - particle->position.x;
     particle->vx = inertia_weight*particle->vx + cognitive_parameter*random01()*local_diff + social_parameter*random01()*global_diff;
+    if (particle->vx > velocity_clamp*bench_fun_bound(func))
+        particle->vx = velocity_clamp*bench_fun_bound(func);
 }
 
 /**
  * Compute the new Y component of the velocity of a particle
 **/
-void compute_velocity_Y(particle_t *particle, position_t global_min) {
+void compute_velocity_Y(particle_t *particle, position_t global_min, string func) {
     float local_diff = particle->local_min.y - particle->position.y;
-    cout << "local diff: " << local_diff << endl;
     float global_diff = global_min.y - particle->position.y;
-    cout << "global diff: " << global_diff << endl;
     particle->vy =  inertia_weight*particle->vy + cognitive_parameter*random01()*local_diff + social_parameter*random01()*global_diff;
+    if (particle->vy > velocity_clamp*bench_fun_bound(func))
+        particle->vy = velocity_clamp*bench_fun_bound(func);
 }
 
 /**
  * Update the velocity of a particle
  */
-void update_velocity(particle_t *particle, position_t global_min) {
-    // cout << particle->vx << endl;
-    compute_velocity_X(particle, global_min);
-    // cout << particle->vx << endl;
-    compute_velocity_Y(particle, global_min);
+void update_velocity(particle_t *particle, position_t global_min, string func) {
+    compute_velocity_X(particle, global_min, func);
+    compute_velocity_Y(particle, global_min, func);
 }
 
 /**
@@ -145,17 +169,23 @@ void update_global(swarm_t *swarm, string func) {
 /**
  * Computes the initial position of the particles in the search space
  */
-swarm_t *initialize_swarm(int n_particles, string func) {
+swarm_t *init_swarm(int n_particles, string func, string init_type) {
     swarm_t *swarm = new swarm_t;
     float domain_max = bench_fun_bound(func);
     float domain_min = -domain_max;
     swarm->particles = new particle_t[n_particles];
-
+    swarm->n_particles = n_particles;
     for (int i=0; i<(int)sqrt(n_particles); i++) {
         for (int j=0; j<(int)sqrt(n_particles); j++) {
             int index = i*(int)sqrt(n_particles)+j;
-            swarm->particles[index].position.x = domain_min + i*2*domain_max/(float)sqrt(n_particles);
-            swarm->particles[index].position.y = domain_min + j*2*domain_max/(float)sqrt(n_particles);
+            if (init_type == "uniform") {
+                swarm->particles[index].position.x = domain_min + i*2*domain_max/(float)sqrt(n_particles);
+                swarm->particles[index].position.y = domain_min + j*2*domain_max/(float)sqrt(n_particles);
+            }
+            else if (init_type == "random") {
+                swarm->particles[index].position.x = randAB(domain_min, domain_max);
+                swarm->particles[index].position.y = randAB(domain_min, domain_max);
+            }
             swarm->particles[index].vx = 0;
             swarm->particles[index].vy = 0;
             swarm->particles[index].local_min = swarm->particles[index].position;
@@ -164,9 +194,7 @@ swarm_t *initialize_swarm(int n_particles, string func) {
             else
                 if (compute_bench_fun(swarm->particles[index].local_min, func) < compute_bench_fun(swarm->global_min, func)) {
                     swarm->global_min = swarm->particles[index].position;
-                    cout << "sargio\n";
                 }
-            swarm->n_particles = n_particles;
         }
     }
     return swarm;
