ec91df94ee63bcb6b55af597210c118664047097
gdrax <gioele.bert@gmail.com>
2019-10-04 16:59:26 +0200

    Makefile

--- Progetto/function_time.sh
+++ Progetto/function_time.sh
@@ -4,10 +4,12 @@ SPHERE=0
 MATYAS=0
 HIMMEL=0
 
+EPOCHS=$1
+
 #mean computation time of the three functions over 10k trials
-for n in {0..100}; do SPHERE=$((${SPHERE}+$(./sequential sphere random 1 10000))); done
-echo sphere: $((${SPHERE}/10000)) usec;
-for n in {0..100}; do MATYAS=$((${MATYAS}+$(./sequential matyas random 1 10000))); done
-echo matyas: $((${MATYAS}/10000)) usec;
-for n in {0..100}; do HIMMEL=$((${HIMMEL}+$(./sequential himmel random 1 10000))); done
-echo himmel: $((${HIMMEL}/10000)) usec;
+for n in {0..100}; do SPHERE=$((${SPHERE}+$(./sequential sphere random 1 $EPOCHS))); done
+echo sphere: $((SPHERE/EPOCHS)) usec;
+for n in {0..100}; do MATYAS=$((${MATYAS}+$(./sequential matyas random 1 $EPOCHS))); done
+echo matyas: $((MATYAS/EPOCHS)) usec;
+for n in {0..100}; do HIMMEL=$((${HIMMEL}+$(./sequential himmel random 1 $EPOCHS))); done
+echo himmel: $((HIMMEL/EPOCHS)) usec;
--- Progetto/sequential.cpp
+++ Progetto/sequential.cpp
@@ -6,31 +6,31 @@ using namespace std;
 
 int main(int argc, char const *argv[]) {
 
-    if (check_arg(argc, argv) == -1)
-        return -1;
-    
-    string target_func = argv[1];
-    string init_type = argv[2];
-    int n_particles = atoi(argv[3]);
-    int epochs = atoi(argv[4]);
+	if (check_arg(argc, argv) == -1)
+		return -1;
+
+	string target_func = argv[1];
+	string init_type = argv[2];
+	int n_particles = atoi(argv[3]);
+	int epochs = atoi(argv[4]);
 
 
-    swarm_t *swarm = init_swarm(n_particles, target_func, init_type);
+	swarm_t *swarm = init_swarm(n_particles, target_func, init_type);
 
 //    print_swarm(swarm, target_func);
 
-    {
-        utimer u("sequential");
+	{
+		utimer u("sequential");
 
-        for (int i = 0; i < epochs; i++) {
+		for (int i = 0; i < epochs; i++) {
 //            cout << "Iteration" << i << ": global_min = " << compute_bench_fun(swarm->global_min, target_func) << "\n";
-            for (int j = 0; j < swarm->n_particles; j++) {
-                update_velocity(&(swarm->particles[j]), swarm->global_min, target_func);
-                update_local(&(swarm->particles[j]), target_func);
-            }
-            update_global(swarm, target_func);
-        }
-        print_swarm(swarm, target_func);
-    }
-    return 0;
+			for (int j = 0; j < swarm->n_particles; j++) {
+				update_velocity(&(swarm->particles[j]), swarm->global_min, target_func);
+				update_local(&(swarm->particles[j]), target_func);
+			}
+			update_global(swarm, target_func);
+		}
+//		print_swarm(swarm, target_func);
+	}
+	return 0;
 }
