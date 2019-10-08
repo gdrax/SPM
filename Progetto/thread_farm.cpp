fda5164c74d3198ceaa2346e790038c5945443b5
gdrax <gioele.bert@gmail.com>
2019-10-08 18:42:14 +0200

    another parallel implementation: queue troubles

---
+++ Progetto/thread_farm.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <thread>
+#include <vector>
+#include "threads.hpp"
+#include "utimer.cpp"
+
+int main(int argc, char *argv[]) {
+
+	if (check_arg(argc, argv) == -1)
+		return -1;
+	if (argc < 6) {
+		cout << "USAGE: " << argv[0] << " [function_name] [init_type] [n. of particles] [n. iterations] [n. threads]\n";
+		return -1;
+	}
+	if (atoi(argv[5]) < 1) {
+		cout << "Number of threads must be positive\n";
+		return -1;
+	}
+
+	string target_func = argv[1];
+	string init_type = argv[2];
+	int n_particles = atoi(argv[3]);
+	int epochs = atoi(argv[4]);
+	int n_threads = atoi(argv[5]);
+
+	swarm_t *swarm = init_swarm(n_particles, target_func, init_type);
+	g_queue<particle_set_t> work_queue = new g_queue;
+	int work = 0;
+
+	//initialize threads
+	vector<thread*> threads;
+	vector<Worker_farm*> workers;
+	Server_farm server = new Server_farm(swarm, epochs, target_func, work, n_threads, work_queue);
+
+	for (int i=0; i<n_threads; i++) {
+		workers.push_back(new Worker_farm(i, work_queue, swarm, target_func, work));
+	}
+
+	{
+		utimer u("thread_farm");
+
+		//run threads
+		for (auto w: workers) {
+			threads.push_back(w->run());
+		}
+
+		threads.push_back(server.run());
+
+		//join threads
+		for (auto t: threads) {
+			t->join();
+		}
+		print_swarm(swarm, target_func);
+	}
+	return 0;
+}
--- Progetto/threads.hpp
+++ Progetto/threads.hpp
@@ -83,27 +83,30 @@ public:
 
 class Worker_farm {
 private:
-	g_queue<input>& input_queue;
+	g_queue<particle_set_t*>& input_queue;
 	int id;
-	swarm_t swarm;
+	swarm_t *swarm;
 	string target_func;
+	int work;
 
 public:
-	Worker_farm(int i, g_queue q, swarm_t s, string tf) {
+	Worker_farm(int i, g_queue<particle_set_t*>& q, swarm_t *s, string tf, int w) {
 		this->input_queue = q;
 		this->id = i;
 		this->swarm = s;
 		this->target_func = tf;
+		this->work = w;
 	}
 
 	thread *run() {
 		auto body = [&] () {
-			particle_set_t particle_set = input_queue.pop();
-			while(particle_set.start != -1 && particle_set.end != -1) {
-				for (int i = this->particle_set->start; i <= this->particle_set->end; i++) {
+			particle_set_t *particle_set = this->input_queue.pop();
+			while(particle_set->start != -1 && particle_set->end != -1) {
+				for (int i = particle_set->start; i <= particle_set->end; i++) {
 					update_velocity(&(this->swarm->particles[i]), this->swarm->global_min, this->target_func);
 					update_local(&(this->swarm->particles[i]), this->target_func);
 				}
+				work++;
 			}
 			return;
 		};
@@ -113,23 +116,45 @@ public:
 
 class Server_farm {
 private:
-	swarm_t swarm;
+	swarm_t *swarm;
 	int epochs;
 	string target_func;
+	int work;
+	int n_threads;
+	g_queue<particle_set_t*> work_queue;
+	int n_particles;
 
 public:
-	Server_farm(swarm_t s, int e, string tf) {
+	Server_farm(swarm_t *s, int e, string tf, int w, int nt, g_queue<particle_set_t*> wq, int np) {
 		this->swarm = s;
 		this->epochs = e;
 		this->target_func = tf;
+		this->work = w;
+		this->n_threads = nt;
+		this->work_queue = wq;
+		this->n_particles = np;
 	}
 
 	thread *run() {
 		auto body = [&] () {
-			while (epochs > 0) {
-				//coordinating stuff
-				update_global(this->swarm, this->target_func);
-				epochs --;
+			while (this->epochs > 0) {
+				if (this->work == 0) {
+					for (int i = 0; i < this->n_threads; i++) {
+						particle_set_t *particle_set_i = get_particles_set(this->n_threads, this->n_particles, i);
+						this->work_queue.push(particle_set_i);
+					}
+				}
+				if (this->work == this->n_threads) {
+					update_global(this->swarm, this->target_func);
+					this->work = 0;
+					this->epochs--;
+				}
+			}
+			particle_set_t *end_set = new particle_set_t;
+			end_set->start = -1;
+			end_set->end = -1;
+			for (int i=0; i<this->n_threads; i++) {
+				this->work_queue.push(end_set);
 			}
 			return;
 		};
