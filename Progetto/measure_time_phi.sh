64a4274c2ddd80967c4e3ee103090144de93b323
gdrax <gioele.bert@gmail.com>
2019-10-16 16:16:41 +0200

    script

---
+++ Progetto/measure_time_phi.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+PARTICLES=$1
+EPOCHS=$2
+THREADS=$3
+TIME=0
+
+#mean computation time of PARTICLES and EPOCHS with THREADS over 100 trials
+echo Computing $PARTICLES particles over $EPOCHS epochs...
+
+for ((i=1; i<THREADS+1; i*=2)); do
+  for n in {0..100}; do
+    TIME=$(($TIME+$(./thread_pool sphere random $PARTICLES $EPOCHS $((i)))))
+  done;
+  echo $((i)) thread: $((TIME/100)) usec
+  TIME=0
+done
