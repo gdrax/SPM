7be7dd393429f83ce17bf0cc066a8343104c37ed
gdrax <gioele.bert@gmail.com>
2019-10-18 12:35:47 +0200

    makefiling stuff

--- Progetto/measure_time.sh
+++ Progetto/measure_time.sh
@@ -4,6 +4,7 @@ PARTICLES=$1
 EPOCHS=$2
 THREADS=$3
 TIME=0
+OUTPUT=./local_times.txt
 
 #mean computation time of PARTICLES and EPOCHS with THREADS over 100 trials
 echo Computing $PARTICLES particles over $EPOCHS epochs...
@@ -12,6 +13,6 @@ for ((i=1; i<THREADS+1; i++)); do
   for n in {0..100}; do
     TIME=$(($TIME+$(./thread_pool sphere random $PARTICLES $EPOCHS $((i)))))
   done;
-  echo $((i)) thread: $((TIME/100)) usec
+  echo $((i)) thread: $((TIME/100)) usec >> $OUTPUT
   TIME=0
 done
