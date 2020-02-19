e21d8e9b66235e6e03583549236401a454692cc8
Gioele Bertoncini <gioele.bert@gmail.com>
2020-02-19 17:01:44 +0100

    plot script

--- Project (Application: PSO)/spm.py
+++ Project (Application: PSO)/spm.py
@@ -6,6 +6,7 @@ multi_thread_times = [132243975, 66475635, 34874273, 17481113, 8765808, 4136962,
 ff_times = [133594345, 71319961, 37079476, 19891571, 11343719, 7012673, 5248750, 5079444, 6302879]
 ideal_times = [sequential_time / 2 ** i for i in range(0, 9)]
 numbers = [1, 2, 4, 8, 16, 32, 64, 128, 256]
+numbers_less = [1, 2, 4, 8, 16, 32, 64]
 
 
 def plot_times():
@@ -32,6 +33,20 @@ def plot_speedup():
     plt.plot(numbers, multi_thread_speedup, label='multi-thread', linestyle=':', color='k')
     plt.plot(numbers, ff_speedup, label='fast-flow', linestyle='-.', color='k')
     plt.xscale('log', basex=2)
+    plt.xticks([1, 2, 4, 8, 16, 32, 64, 128, 256], [1, 2, 4, 8, 16, 32, 64, 128, 256])
+    plt.xlabel('N. of workers')
+    plt.ylabel('Speedup')
+    plt.legend('True', labels=('Ideal', 'Multi-thread', 'fast-flow'))
+    plt.show()
+
+def plot_speedup_linear():
+    multi_thread_speedup = [sequential_time / multi_thread_times[i] for i in range(0, 9)]
+    ff_speedup = [sequential_time / ff_times[i] for i in range(0, 9)]
+    ideal_speedup = [sequential_time / ideal_times[i] for i in range(0, 9)]
+    plt.figure(num=None, figsize=(8, 6), dpi=80, facecolor='w', edgecolor='k')
+    plt.plot(numbers, ideal_speedup, label='ideal', linestyle='-', color='k')
+    plt.plot(numbers, multi_thread_speedup, label='multi-thread', linestyle=':', color='k')
+    plt.plot(numbers, ff_speedup, label='fast-flow', linestyle='-.', color='k')
     plt.xticks([1, 2, 4, 8, 16, 32, 64, 128, 256], [1, 2, 4, 8, 16, 32, 64, 128, 256])
     plt.xlabel('N. of workers')
     plt.ylabel('Speedup')
@@ -70,4 +85,4 @@ def plot_efficiency():
     plt.legend('True', labels=('Ideal', 'Multi-thread', 'fast-flow'))
     plt.show()
 
-plot_efficiency()
+plot_speedup_linear()
